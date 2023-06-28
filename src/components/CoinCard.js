636deac14859c6eb378851037d7275e9a6197974
mrcbq <marcobaquero@gmail.com>
2023-06-28 16:52:49 -0500

    Add arrow component for cards and styles to this component

--- src/components/CoinCard.js
+++ src/components/CoinCard.js
@@ -1,17 +1,21 @@
 import PropTypes from 'prop-types';
+import { BsArrowRightCircle } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
 
 export default function CoinCard({
   icon, price, symbol, id,
 }) {
   return (
-    <Link to={`/coin/${id}`}>
+    <div>
+      <Link to={`/coin/${id}`} className="arrow-container">
+        <BsArrowRightCircle />
+      </Link>
       <div className="coin-card" data-testid="coin-card">
         <h2>{symbol}</h2>
         <img src={icon} alt={symbol} />
         <p>{parseFloat(price).toFixed(2)}</p>
       </div>
-    </Link>
+    </div>
   );
 }
 
